fdef635ad5e804336969fb51c1fc7d908b7f9912
Maciej <macbednarczyk@gmail.com>
2022-05-23 15:18:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# interaction_controllers
+This package contains the implementation of the following controllers for `ros2_control` that are used for physical human-robot-environment interaction control.
+## Impedance Controller
+## Admittance Controller 
+## Model Predictive Impedance Controller (MPIC)
+```
+@inproceedings{ Bednarczyk_MPIC_2020, 
+ 	title={Model Predictive Impedance Control},
+  	author={Bednarczyk, Maciej and Omran, Hassan and Bayle, Bernard}, 
+	booktitle={2020 IEEE International Conference on Robotics and Automation (ICRA)},	
+	place={Paris, France}, 
+  	year={2020}, 
+  	month={May},
+ 	pages={4702-4708} 
+}
+```
+## Variable Impedance Controller With Passivity Guarantees (VIC)
+```
+@inproceedings{ Bednarczyk_VIC_2020, 
+	title={Passivity Filter for Variable Impedance Control},
+	author={Bednarczyk, Maciej and Omran, Hassan and Bayle, Bernard},
+	booktitle={2020 IEEE/RSJ International Conference on Intelligent Robots and Systems (IROS)},
+	place={Las Vegas, USA}, 
+	year={2020}, 
+	month={Oct},
+	pages={7159-7164}
+} 
+```
+## Contacts ##
+![icube](https://icube.unistra.fr/fileadmin/templates/DUN/icube/images/logo.png)
+
+[ICube Laboratory](https://plateforme.icube.unistra.fr), [University of Strasbourg](https://www.unistra.fr/), France
+
+__Maciej Bednarczyk:__ [m.bednarczyk@unistra.fr](mailto:m.bednarczyk@unistra.fr), @github: [mcbed](mailto:macbednarczyk@gmail.com)
---
+++ model_predictive_impedance_controller/include/model_predictive_impedance_controller/model_predictive_impedance_controller.hpp
@@ -0,0 +1,97 @@
+// Copyright 2022, ICube Laboratory, University of Strasbourg
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MODEL_PREDICTIVE_IMPEDANCE_CONTROLLER__IMPEDANCE_CONTROLLER_HPP_
+#define MODEL_PREDICTIVE_IMPEDANCE_CONTROLLER__IMPEDANCE_CONTROLLER_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "controller_interface/controller_interface.hpp"
+#include "model_predictive_impedance_controller/visibility_control.h"
+#include "rclcpp/subscription.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+#include "realtime_tools/realtime_buffer.h"
+#include "trajectory_msgs/msg/joint_trajectory.hpp"
+
+namespace model_predictive_impedance_controller
+{
+using CmdType = trajectory_msgs::msg::JointTrajectory;
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+/**
+ * \brief Impedance controller for a set of joints.
+ *
+ * This class computes the torque given by the impedance relationship and applies it to the defined joints.
+ *
+ * \param joints Names of the joints to control.
+ * \param interface_name Name of the interface to command.
+ *
+ * Subscribes to:
+ * - \b proxy (trajectory_msgs::msg::JointTrajectory) : The trajectory of the proxy to follow.
+ */
+class ModelPredictiveImpedanceController : public controller_interface::ControllerInterface
+{
+public:
+  MODEL_PREDICTIVE_IMPEDANCE_CONTROLLER_PUBLIC
+  ModelPredictiveImpedanceController();
+
+  MODEL_PREDICTIVE_IMPEDANCE_CONTROLLER_PUBLIC
+  CallbackReturn on_init() override;
+
+  /**
+   * @brief command_interface_configuration This controller requires the effort command
+   * interfaces for the controlled joints
+   */
+  MODEL_PREDICTIVE_IMPEDANCE_CONTROLLER_PUBLIC
+  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+
+  /**
+   * @brief command_interface_configuration This controller requires the position and velocity
+   * state interfaces for the controlled joints
+   */
+  MODEL_PREDICTIVE_IMPEDANCE_CONTROLLER_PUBLIC
+  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
+  MODEL_PREDICTIVE_IMPEDANCE_CONTROLLER_PUBLIC
+  CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+
+  MODEL_PREDICTIVE_IMPEDANCE_CONTROLLER_PUBLIC
+  CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+
+  MODEL_PREDICTIVE_IMPEDANCE_CONTROLLER_PUBLIC
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+
+  MODEL_PREDICTIVE_IMPEDANCE_CONTROLLER_PUBLIC
+  controller_interface::return_type update(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+protected:
+  std::vector<std::string> joint_names_;
+  std::vector<double> stiffness_;
+  std::vector<double> damping_;
+  std::vector<double> mass_;
+
+
+  realtime_tools::RealtimeBuffer<std::shared_ptr<CmdType>> rt_command_ptr_;
+  rclcpp::Subscription<CmdType>::SharedPtr joints_command_subscriber_;
+
+  std::string logger_name_;
+
+};
+
+}  // namespace model_predictive_impedance_controller
+
+#endif  // MODEL_PREDICTIVE_IMPEDANCE_CONTROLLER__IMPEDANCE_CONTROLLER_HPP_
